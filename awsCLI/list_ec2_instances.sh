2bb261fbf58126434941a95932f7bf16981a2236
franciscoboll <franciscoboll-externo@gmail.com>
2024-09-19 23:43:28 -0300

    added name retrieval to list_ec2

--- awsCLI/list_ec2_instances.sh
+++ awsCLI/list_ec2_instances.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# List EC2 instances with instance ID and state
-aws ec2 describe-instances --query "Reservations[*].Instances[*].[InstanceId, State.Name]" --output table
+# Fetch all EC2 instances and display instance ID, Name tag (if available), and state
+aws ec2 describe-instances \
+  --query 'Reservations[*].Instances[*].[InstanceId, State.Name, Tags[?Key==`Name`].Value | [0]]' \
+  --output table
